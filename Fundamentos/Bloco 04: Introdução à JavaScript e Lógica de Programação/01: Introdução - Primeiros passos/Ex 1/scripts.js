0057e212ab04bfd579d9c4ca259733eb3256fabc
Danielle Gazarini <daniellegazarini@gmail.com>
2022-05-15 17:53:57 -0300

    ex 1

---
+++ Fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - Primeiros passos/Ex 1/scripts.js
@@ -0,0 +1,11 @@
+ const myName = "Danielle";
+ const birthCity = "São Paulo";
+ var birthYear = 1989;
+
+ console.log(myName);
+ console.log(birthCity);
+ console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
